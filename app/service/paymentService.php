e86ba64eef76c0d3ea7fb118abba115394f029d1
unknown <domas.sab@gmail.com>
2018-12-03 17:42:55 +0200

    small update

--- app/service/paymentService.php
+++ app/service/paymentService.php
@@ -22,37 +22,37 @@ class paymentService
 		return $jsonDecoded;
 	}
 
-	public function counter($array)
+	public function counter($json)
 	{
 		$smsToSend = array();
-		$numbersOverPrice = array();
-		$smsInput = $array;
-		$price = $smsInput['required_income'];
-		$maxSms = $smsInput['max_messages'];
+		$input = $json;
+		$price = $input['required_income'];
+		$maxSms = $input['max_messages'];
 		$errorMsg = '';
 		$number = 0;
 
-		for ($i = count($smsInput['sms_list']) - 1; $i >= 0;) { 
-			$number += $smsInput['sms_list'][$i]['income'];
-			$smsToSend[] += $smsInput['sms_list'][$i]['price'];
+		for ($i = count($input['sms_list']) - 1; $i >= 0;) { 
+			$number += $input['sms_list'][$i]['income'];
+			$smsToSend[] += $input['sms_list'][$i]['price'];
 			$maxSms -= 1;
 
 			if ($number > $price && $i != 0) {
-				$numbersOverPrice[] = $number;
-				$number -= $smsInput['sms_list'][$i]['income'];
+				$number -= $input['sms_list'][$i]['income'];
 				array_pop($smsToSend);
 				$i--;
 			} else if ($number == $price) {
 				break;
 			} else if ($number != $price && $i == 0) {
 				break;
-			} else if ($maxSms == 0) {
+			} else if ($maxSms == 0 && $number != $price) {
 				$errorMsg = 'Sms limit is too low to pay the price';
 				break;
 			}
 		}
 		if ($errorMsg == '') {
 			print_r(json_encode($smsToSend));
+			print_r(' Paid: ' . $number);
+			return json_encode($smsToSend);
 		} else {
 			echo $errorMsg;
 		}
